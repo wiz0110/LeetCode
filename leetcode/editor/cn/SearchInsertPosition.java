8886379c593d954c5b6c4e8c4fb27d0b0f494040
一鸣666 <chenyiming01@ecut.edu.cn>
2022-01-05 23:56:30 +0800

    变更

--- leetcode/editor/cn/SearchInsertPosition.java
+++ leetcode/editor/cn/SearchInsertPosition.java
@@ -64,7 +64,16 @@
       //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int searchInsert(int[] nums, int target) {
-int
+        int left = 0, right = nums.length;
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            if (target <= nums[mid]){
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
