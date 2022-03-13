14e1936d49c488738239204f81d3b5b657781144
一鸣666 <chenyiming01@ecut.edu.cn>
2022-03-13 14:10:47 +0800

    变更

---
+++ leetcode/editor/cn/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,74 @@
+  //给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "abcabcbb"
+//输出: 3 
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "bbbbb"
+//输出: 1
+//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: s = "pwwkew"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= s.length <= 5 * 10⁴ 
+// s 由英文字母、数字、符号和空格组成 
+// 
+// Related Topics 哈希表 字符串 滑动窗口 👍 7089 👎 0
+
+  
+  package com.shuzijun.leetcode.editor.en;
+
+  import java.util.HashMap;
+  import java.util.Map;
+  import java.util.*;
+
+  public class LongestSubstringWithoutRepeatingCharacters{
+      public static void main(String[] args) {
+           Solution solution = new LongestSubstringWithoutRepeatingCharacters().new Solution();
+      }
+      //leetcode submit region begin(Prohibit modification and deletion)
+      class Solution {
+          public int lengthOfLongestSubstring(String s) {
+              int n = s.length();
+
+              int[] last = new int[128];
+              Arrays.fill(last, -1);
+
+              int start = 0;
+              int ans = 0;
+
+              for (int i = 0; i < n; i++){
+                  int index = s.charAt(i);
+                  start = Math.max(start, last[index] + 1);
+                  ans = Math.max(ans, i - start + 1);
+                  last[index] = i;
+              }
+              return ans;
+          }
+      }
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+  }
--- leetcode/editor/cn/TrappingRainWater.java
+++ leetcode/editor/cn/TrappingRainWater.java
@@ -42,21 +42,21 @@ class Solution {
         int left = 0, right = height.length - 1;
         int l_max = 0, r_max = 0;
 
-        int res = 0;
+        int ans = 0;
 
         while (left < right) {
             l_max = Math.max(l_max, height[left]);
             r_max = Math.max(r_max, height[right]);
 
-            if (l_max < r_max) {
-                res += l_max - height[left];
+            if (l_max < r_max){
+                ans += l_max - height[left];
                 left++;
             } else {
-                res += r_max - height[right];
+                ans += r_max - height[right];
                 right--;
             }
         }
-        return res;
+        return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
